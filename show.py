048bb8996ac820d6ea8f34a12b1f3ab019d8f78c
nanjizhiyin <nanjizhiyin@163.com>
2018-08-21 13:39:55 +0800

    保存到mongo中

--- show.py
+++ show.py
@@ -4,11 +4,20 @@
 """This script parse stock info"""
 
 import tushare as ts
+import pymongo
+import json
 
 def get_all_price(code_list):
     '''process all stock'''
     df = ts.get_realtime_quotes(code_list)
     print(df)
+
+
+    conn = pymongo.MongoClient('10.10.1.169', port=27017,tz_aware=False)
+    db = conn.get_database("admin")
+    db.authenticate("root", "5211314")
+    db = conn.get_database("stock")
+    db.tickdata.insert(json.loads(df.to_json(orient='records')))
 
 if __name__ == '__main__':
     STOCK = ['300648',
