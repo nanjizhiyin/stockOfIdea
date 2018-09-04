cc70bce20a766603adc4c0feee16b80ecb96737f
nanjizhiyin <nanjizhiyin@163.com>
2018-09-04 16:08:08 +0800

    更换kdj,macd算法

--- stocklist.py
+++ stocklist.py
@@ -4,11 +4,13 @@
 import tushare as ts
 import oneStock
 import mail
+import string
 
 def stockList():
     # 股票列表
     df = ts.get_stock_basics()
-
+    # 市盈率
+    kPe = 50
     # 邮件内容
     mailText = ""
     # 输出一个字段
@@ -16,11 +18,15 @@ def stockList():
         print(index +"--"+ row['name'])
         # 市盈率
         pe = row['pe']
-        if pe > 50:
-            print('市盈率大于50')
+        if pe > kPe:
+            print('市盈率大于'+str(kPe))
+            continue
+        name = row['name']
+        if name.find('ST') > -1:
+            print("抛弃st")
             continue
 
-        returnStr = oneStock.analysisStock(index,row['name'])
+        returnStr = oneStock.analysisStock(index,name)
         if returnStr != None:
             mailText += returnStr+'\n'
 
@@ -28,7 +34,7 @@ def stockList():
     if mailText != "" :
         mail.sendMail(mailText)
     else:
-        mail.sendMail("没有可买的股票")
+        mail.sendMail("没有可买的")
 
 if __name__ == '__main__':
     stockList()
